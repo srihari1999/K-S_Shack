8bcc48e2bf4624d60fe900ec2668fefc4412559f
srihari1999 <42672656+srihari1999@users.noreply.github.com>
2021-02-13 13:24:26 +0530

    KS Shack

---
+++ footer.php
@@ -0,0 +1,25 @@
+<!--footer-->
+<footer class="footer" id="footer">
+ <div class="container"><br>
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <div class="social-icon">
+        <a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a>
+        <!-- <a href="index.php"><i class="fa fa-facebook"></i></a> -->
+        <a href="https://twitter.com/"><i class="fa fa-twitter"></i></a>
+        <!-- <a href="index.php"><i class="fa fa-twitter"></i></a> -->
+        <!-- <a href="index.php"><i class="fa fa-pinterest"></i></a>
+        <a href="index.php"><i class="fa fa-rss"></i></a> -->
+      </div>
+      <div class="copyright">
+        <p class="white"> Copyright &copy; <b>K-S Shack</b></p>
+        <p><i class="fa fa-phone"></i> +91 9856123568 &nbsp;<i class="fa fa-envelope-o"></i> ksshack@gmail.com</p>
+      </div>
+    </div>
+  </div>
+ </div>
+</footer>
+<!--end of footer-->
+</body>
+<!---end of body-->
+</html>
---
+++ includes/signup.inc.php
@@ -0,0 +1,94 @@
+<?php
+
+//between function.. checks if the characters are within the limits we set
+
+function between($val, $x, $y){
+    $val_len = strlen($val);
+    return ($val_len >= $x && $val_len <= $y)?TRUE:FALSE;
+}
+
+if(isset($_POST['signup-submit'])) {//check if it has said on the page via the contribution
+
+    
+    require 'dbh.inc.php';
+    
+    $username = $_POST['uid'];
+    $email = $_POST['mail'];
+    $password = $_POST['pwd'];
+    $passwordRepeat = $_POST['pwd-repeat'];
+    
+    
+    if(empty($username) || empty($email) || empty($password) || empty($passwordRepeat)) {
+        header("Location: ../index.php?error=emptyfields");
+        exit();
+    }
+    else if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)){
+        header("Location: ../index.php?error=invalidemailusername");
+        exit();  
+    }
+    else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: ../index.php?error=invalidemail");
+        exit();
+    }
+    else if(!preg_match("/^[a-zA-Z0-9]*$/", $username) || !between($username,4,20)) {
+        header("Location: ../index.php?error=invalidusername");
+        exit();
+    }
+    else if(!between($password,6,20)) {
+        header("Location: ../index.php?error=invalidpassword");
+        exit();
+    }
+    else if($password !== $passwordRepeat){
+       header("Location: ../index.php?error=passworddontmatch");
+       exit();
+    }
+   else {
+       
+       $sql = "SELECT uidUsers, emailUsers FROM users WHERE uidUsers=? OR emailUsers=?";
+       $stmt = mysqli_stmt_init($conn);
+       if(!mysqli_stmt_prepare($stmt, $sql)){
+           header("Location: ../index.php?error=error1");
+           exit();
+       }
+       else {
+           mysqli_stmt_bind_param($stmt, "ss", $username, $email);     //check if there is an email and we are already dragging
+           mysqli_stmt_execute($stmt);
+           mysqli_stmt_store_result($stmt);
+           $resultCheck = mysqli_stmt_num_rows($stmt);
+             if($resultCheck != 0){
+                header("Location: ../index.php?error=usernameemailtaken");
+                exit();
+           }
+          
+           
+           else {
+            $sql = "INSERT INTO users(uidUsers, emailUsers, pwdUsers) VALUES(?, ?, ?)";
+            $stmt = mysqli_stmt_init($conn);
+                 if(!mysqli_stmt_prepare($stmt, $sql)){
+                    header("Location: ../index.php?error=error2");
+                    exit();
+                }
+                else {
+                    $hashedPwd = password_hash($password, PASSWORD_DEFAULT);    //encrypting password
+                            
+                            
+                    mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
+                    mysqli_stmt_execute($stmt);
+                    header("Location: ../index.php?signup=success");
+                    exit();
+                }
+                
+           }
+           
+       }
+   } 
+   //Close the connection
+   mysqli_stmt_close($stmt);
+   mysqli_close($conn);
+   
+}
+else{
+    header("Location: ../index.php");
+    exit();
+    
+}
